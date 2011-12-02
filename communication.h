18628e4154b518df6cac5c5287814a58a2e50f03
Andy Guenin <andyguenin@gmail.com>
2011-12-02 14:51:53 -0500

    added the wireless commands as define constants

--- communication.h
+++ communication.h
@@ -5,3 +5,13 @@ volatile bool wireless_buffer_f;
 void init_communication(int c, int a, int l);
 void send_message(char* c);
 void send_message_to_bot(char* c, char address);
+
+
+#define COMMTEST	0xA0
+#define PLAY		0xA1
+#define GOALA		0xA2
+#define GOALB		0xA3
+#define PAUSE		0xA4
+#define DETANGLE	0xA5
+#define HALFTIME	0xA6
+#define GAMEOVER	0xA7
